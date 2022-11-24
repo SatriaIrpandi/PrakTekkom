761b9549c10b0cbca153e3f13793250599d04c8a
SatriaIrpandi <117892851+SatriaIrpandi@users.noreply.github.com>
2022-11-24 12:46:53 +0700

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,12 @@
+a= int(input("Bilangan pertama = "))
+b= int(input("Bilangan kedua   = "))
+
+penjumlahan=a+b
+perkalian  =a*b
+tambah     =a+b+0.5
+kali       =a*b*0.5
+
+print("Hasil Penjumlahan semua bilangan =",penjumlahan)
+print("Hasil Perkalian semua bilangan =",perkalian)
+print("Jika ditambah 0.5 hasilnya =",tambah)
+print("Jika dikali 0.5 hasilnya =",kali)
